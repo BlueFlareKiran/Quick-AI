c2b693cfb88e0c2c66588e7b8b46b6cd8960aea7
Kiran5655 <142571315+Kiran5655@users.noreply.github.com>
2025-08-30 22:20:18 +0530

    Day 1 after a while

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
 
 export default function Home() {
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+   <>
+   <Navbar />
+   <Hero />
+   <AiTools />
+   <Testimonial />
+   </>
   )
 }
